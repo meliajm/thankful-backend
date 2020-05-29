6045c93a717ecb5c62ad22cd267e9c04c494a754
github username <meliajm@gmail.com>
2020-05-29 09:21:48 -0700

    added cookies, current_user, logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::API include ActionController::Cookies
+    
+    def current_user
+        User.find(session[:user_id])
+    end
+
+    def logged_in?
+        !!session[:user_id]
+    end
 end
+
